420369623344531bf998be505abd14ccf9ee161a
adour-zc21s <wahid.asimetris@gmail.com>
2026-09-17 11:49:06 +0700

    back to sion to reack

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sion App',
+      title: 'RIEK App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
